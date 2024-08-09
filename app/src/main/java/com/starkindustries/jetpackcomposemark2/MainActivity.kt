6d7fa3276d5bb752a394c42d5ea0dcc57fc9d52a
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-08-10 00:03:45 +0530

    Modifiers done

--- app/src/main/java/com/starkindustries/jetpackcomposemark2/MainActivity.kt
+++ app/src/main/java/com/starkindustries/jetpackcomposemark2/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.starkindustries.jetpackcomposemark2.ui.theme.JetPackComposeMark2Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +37,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetPackComposeMark2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
-
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun FirstModifier(){
+    Text(text = "Ironman",
+        fontSize = 20.sp,
+        fontWeight = FontWeight.W100,
+        fontStyle = FontStyle.Normal,
+        fontFamily = FontFamily.Monospace,
+        modifier = Modifier
+            .clickable { }
+            .size(200.dp)
+            .background(Color.Blue)
+            .clip(CircleShape)
+            .background(Color.Yellow)
+            .border(10.dp, Color.Red, CircleShape)
     )
 }
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    JetPackComposeMark2Theme {
-        Greeting("Android")
-    }
+fun SecondComposoble(){
+    Image(painter = painterResource(id = R.drawable.ironman_arc_reactor), contentDescription = ""
+    , modifier = Modifier.clickable {  }.
+    clip(CircleShape).
+    border(10.dp,Color.Blue, CircleShape),
+        contentScale = ContentScale.Crop)
 }
